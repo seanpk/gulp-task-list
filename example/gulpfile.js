aa3592ca71f2cd271341301b42cfd21a2416c932
Sean Kennedy <sean@mule.domain>
2014-11-15 02:50:26 -0500

    minor improvements and bug fixing ... the whole "comment-as-description" algorithm needs to be revised

---
+++ example/gulpfile.js
@@ -0,0 +1,26 @@
+/* someday this maybe should be tests ... */
+var gulp = require('gulp');
+
+require('../index.js')(gulp);
+
+// This is a comment
+gulp.task('alpha', function() {
+	console.log('ONE');
+});
+
+// This line won't be included ...
+// Only this line will be in the comment
+gulp.task('beta', function() {
+	console.log('TWO');
+});
+
+/* This _isn't_ a comment! */
+gulp.task('gamma', ['alpha', 'beta'], function() {
+	console.log('THREE gulp-y tasks ... AH! AH! AH!');
+});
+
+var mytask = 'my-task';
+// You'll never see this comment!
+gulp.task(mytask, function() {
+	console.log('shh...');
+});
